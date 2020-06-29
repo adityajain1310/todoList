31ca824977ef2cd81fc476e6b9319b4b728fb353
Aditya Jain <jain13102000@gmail.com>
2020-06-29 16:04:57 +0530

    Adding features like update delete

--- public/todoList.js
+++ public/todoList.js
@@ -1,7 +1,23 @@
 $(function () {
+    let showToDoList = $('#showtodo')
     let newToDOBox = $('#newtodo')
+    let indexBox = $('#index')
     let addToDoBtn = $('#addtodo')
     let todoList = $('#todolist')
+    let updateToDoBtn = $('#updatetodo')
+    let deleteToDoBtn = $('#deletetodo')
+
+    showToDoList.click(function () {
+        $.get(
+            '/todoList',
+            function (data) {
+                todoList.empty();
+                for (todo of data) {
+                    todoList.append("<li>" + todo.task + "</li>")
+                }
+            }
+        )
+    })
 
     addToDoBtn.click(function () {
         let newToDo = newToDOBox.val()
@@ -13,6 +29,21 @@ $(function () {
                 todoList.empty();
                 for (todo of data) {
                     todoList.append("<li>" + todo.task + "</li>")
+                }
+            }
+        )
+    })
+
+    updateToDoBtn.click(function () {
+        let index = indexBox.val()
+        let newToDo = newToDOBox.val()
+        $.patch(
+            '/todoList/' + index,
+            {task: newToDo},
+            function (data) {
+                todoList.empty();
+                for(todo of data) {
+                    todoList.append("<li>" + todo.task + "<li>")
                 }
             }
         )
